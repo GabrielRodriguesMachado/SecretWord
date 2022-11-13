99d718d2e641452db1a7ac63b19d5d549522f889
GabrielRodriguesMachado <gabrm123@gmail.com>
2022-11-13 19:58:40 -0300

    criada lista de palavras que vão ser usadas

---
+++ secret-word/src/data/words.js
@@ -0,0 +1,9 @@
+export const words = {
+  carro: ["Motor", "Rodas", "Portas", "Pneus"],
+  fruta: ["Maçã", "Banana", "Uva", "Morango"],
+  animal: ["Cachorro", "Gato", "Papagaio", "Pato"],
+  objeto: ["Copo", "Garfo", "Faca", "Prato"],
+  cor: ["Vermelho", "Azul", "Verde", "Amarelo"],
+  profissão: ["Médico", "Engenheiro", "Professor", "Padeiro"],
+  esporte: ["Futebol", "Basquete", "Vôlei", "Tênis"],
+};
