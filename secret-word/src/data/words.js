79a1702639016bfbc7fbfae0d1c48a29610feb0e
GabrielRodriguesMachado <gabrm123@gmail.com>
2022-11-13 20:02:05 -0300

    lint configurado

--- secret-word/src/data/words.js
+++ secret-word/src/data/words.js
@@ -1,9 +1,11 @@
-export const words = {
-  carro: ["Motor", "Rodas", "Portas", "Pneus"],
-  fruta: ["Maçã", "Banana", "Uva", "Morango"],
-  animal: ["Cachorro", "Gato", "Papagaio", "Pato"],
-  objeto: ["Copo", "Garfo", "Faca", "Prato"],
-  cor: ["Vermelho", "Azul", "Verde", "Amarelo"],
-  profissão: ["Médico", "Engenheiro", "Professor", "Padeiro"],
-  esporte: ["Futebol", "Basquete", "Vôlei", "Tênis"],
+const words = {
+  carro: ['Motor', 'Rodas', 'Portas', 'Pneus'],
+  fruta: ['Maçã', 'Banana', 'Uva', 'Morango'],
+  animal: ['Cachorro', 'Gato', 'Papagaio', 'Pato'],
+  objeto: ['Copo', 'Garfo', 'Faca', 'Prato'],
+  cor: ['Vermelho', 'Azul', 'Verde', 'Amarelo'],
+  profissão: ['Médico', 'Engenheiro', 'Professor', 'Padeiro'],
+  esporte: ['Futebol', 'Basquete', 'Vôlei', 'Tênis'],
 };
+
+export default words;
