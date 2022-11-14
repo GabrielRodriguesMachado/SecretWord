b1e0910de2925510901cfc495f702571e2c3358b
GabrielRodriguesMachado <gabrm123@gmail.com>
2022-11-14 06:56:51 -0300

    criados os components do projeto

--- secret-word/src/App.jsx
+++ secret-word/src/App.jsx
@@ -1,11 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
+import Game from './components/Game';
+import GameOver from './components/GameOver';
 import StartScreen from './components/StartScreen';
+import wordsList from './data/words';
+
+const stages = [
+  { id: 1, name: 'start' },
+  { id: 2, name: 'game' },
+  { id: 3, name: 'end' },
+];
 
 function App() {
+  const [stage] = useState(stages[0].name);
+  const [words] = useState(wordsList);
+
   return (
     <div className="App">
-      <StartScreen />
+      {stage === 'start' && <StartScreen />}
+      {stage === 'game' && <Game />}
+      {stage === 'end' && <GameOver />}
+      {console.log(words)}
     </div>
   );
 }
